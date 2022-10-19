c58d60239cb993ec5c2c1301a99f3d134ab55945
James-Fraser1 <jdfraser2022@gmail.com>
2022-10-18 21:10:22 -0400

    initial save button functionality created for scheduler

--- Develop/index.js
+++ Develop/index.js
@@ -1,28 +1,7 @@
-const canvas = document.getElementById('canv');
-const ctx = canvas.getContext('2d');
+$(document).ready(function () {
 
-const w = canvas.width = document.body.offsetWidth;
-const h = canvas.height = document.body.offsetHeight;
-const cols = Math.floor(w / 20) + 1;
-const ypos = Array(cols).fill(0);
-
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, w, h);
-
-function matrix () {
-  ctx.fillStyle = '#0001';
-  ctx.fillRect(0, 0, w, h);
-  
-  ctx.fillStyle = '#0f0';
-  ctx.font = '15pt monospace';
-  
-  ypos.forEach((y, ind) => {
-    const text = String.fromCharCode(Math.random() * 128);
-    const x = ind * 20;
-    ctx.fillText(text, x, y);
-    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
-    else ypos[ind] = y + 20;
-  });
-}
-
-setInterval(matrix, 50);
+  $(".saveButton").on("click", function () {
+    var text = $(this).sibling(".description").val();
+    var time = $(this).parent().attr("id");
+  })
+})
